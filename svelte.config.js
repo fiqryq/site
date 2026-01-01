667a682b190b5eecd66ffee0d7ba38598ce5d929
Fiqry choerudin <fiqrychoerudin48@gmail.com>
2026-01-01 10:11:19 +0700

    chore: update adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex, escapeSvelte } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { createHighlighter } from 'shiki';
 
@@ -34,8 +34,6 @@ const highlighter = await createHighlighter({
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: [
 		vitePreprocess(),
 		mdsvex({
@@ -47,12 +45,10 @@ const config = {
 			}
 		})
 	],
-
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'nodejs20.x'
+		})
 	},
 
 	extensions: ['.svelte', '.svx']
